d088b5f4fa1ce2221d6162054325108a1c8b88f8
Shreeja-926 <39011766+Shreeja-926@users.noreply.github.com>
2018-11-27 19:36:00 +0530

    FirstJavaFile created

---
+++ MyFirstJavaFile.java
@@ -0,0 +1,15 @@
+class firstclass
+{
+public void firstmethod()
+{
+System.out.println("Hello World!!!!");
+}
+}
+class shreeja
+{
+public static void main(String args[])
+{
+firstclass fc = new firstclass();
+fc.firstmethod();
+}
+}
